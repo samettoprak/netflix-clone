13109473c79295c9f81511475acca991d7994397
samettoprak <topraksamet41@gmail.com>
2023-05-24 19:46:12 +0300

    first commit

---
+++ src/Firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBmMbhNEsWLeNctTCYLe7VZVpz7SZTSPjQ",
+  authDomain: "netflix-clone-2e880.firebaseapp.com",
+  projectId: "netflix-clone-2e880",
+  storageBucket: "netflix-clone-2e880.appspot.com",
+  messagingSenderId: "334604411966",
+  appId: "1:334604411966:web:aa3e2ded99b5699b719d6f",
+};
---
+++ src/Nav.js
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from "react";
+import "./Nav.css";
+
+function Nav() {
+  const [show, handleShow] = useState(false);
+  const transitionNavBar = () => {
+    if (window.scrollY > 100) {
+      handleShow(true);
+    } else {
+      handleShow(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", transitionNavBar);
+    return () => window.removeEventListener("scroll", transitionNavBar);
+  }, []);
+
+  return (
+    <div className={`nav ${show && "nav_black"}`}>
+      <div className="nav_contents">
+        <img
+          className="nav_logo"
+          src="https://upload.wikimedia.org/wikipedia/commons/7/7a/Logonetflix.png"
+          alt=""
+        ></img>
+        <img
+          className="nav_avatar"
+          src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
+          alt=""
+        ></img>
+      </div>
+    </div>
+  );
+}
+
+export default Nav;
